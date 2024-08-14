eabcca0bc7594f9dd791608c8fa502ba8ffe1509
CodyBense <codybense@gmail.com>
2024-08-14 18:43:21 -0400

    added test list file

--- cmd/list/mkList.go
+++ cmd/list/mkList.go
@@ -1,82 +1,82 @@
-package list
-
-import (
-	"fmt"
-	"os"
-
-	"github.com/CodyBense/todo/cmd/mySql/bubbletea_queries"
-	"github.com/charmbracelet/bubbles/list"
-	tea "github.com/charmbracelet/bubbletea"
-	"github.com/charmbracelet/lipgloss"
-)
-
-var (
-    appStyle = lipgloss.NewStyle().Margin(1, 2)
-
-    titleStyle = lipgloss.NewStyle().
-        Foreground(lipgloss.Color("#FFFDF5")).
-        Background(lipgloss.Color("#25A065")).
-        Padding(0, 1)
-    
-    notDoneStyle = lipgloss.Color("#CC0000")
-    doneStyle = lipgloss.Color("#29FF03")
-)
-
-type item struct {
-	title, desc, done string
-}
-
-func (i item) Title() string       { return i.title }
-func (i item) Description() string { return i.desc }
-func (i item) FilterValue() string { return i.done }
-
-type model struct {
-	list list.Model
-}
-
-func (m model) Init() tea.Cmd {
-	return nil
-}
-
-func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	switch msg := msg.(type) {
-	case tea.KeyMsg:
-		if msg.String() == "ctrl+c" {
-			return m, tea.Quit
-		}
-	case tea.WindowSizeMsg:
-		h, v := appStyle.GetFrameSize()
-		m.list.SetSize(msg.Width-h, msg.Height-v)
-	}
-
-	var cmd tea.Cmd
-	m.list, cmd = m.list.Update(msg)
-	return m, cmd
-}
-
-func (m model) View() string {
-	return appStyle.Render(m.list.View())
-}
-
-func Main() {
-    results := bubbletea_queries.List()
-    items := []list.Item{}
-
-    for _, r := range results {
-        items = append(items, item{title: r["task"], desc: r["id"], done: r["done"]})
-    }
-    
-    delegate := list.NewDefaultDelegate()
-    delegate.Styles.SelectedTitle = delegate.Styles.SelectedTitle.Foreground(notDoneStyle)
-
-	m := model{list: list.New(items, delegate, 0, 0)}
-	m.list.Title = "TODO"
-    m.list.Styles.Title = titleStyle
-
-	p := tea.NewProgram(m, tea.WithAltScreen())
-
-	if _, err := p.Run(); err != nil {
-		fmt.Println("Error running program:", err)
-		os.Exit(1)
-	}
-}
+// package list
+//
+// import (
+// 	"fmt"
+// 	"os"
+//
+// 	"github.com/CodyBense/todo/cmd/mySql/bubbletea_queries"
+// 	"github.com/charmbracelet/bubbles/list"
+// 	tea "github.com/charmbracelet/bubbletea"
+// 	"github.com/charmbracelet/lipgloss"
+// )
+//
+// var (
+//     appStyle = lipgloss.NewStyle().Margin(1, 2)
+//
+//     titleStyle = lipgloss.NewStyle().
+//         Foreground(lipgloss.Color("#FFFDF5")).
+//         Background(lipgloss.Color("#25A065")).
+//         Padding(0, 1)
+//     
+//     notDoneStyle = lipgloss.Color("#CC0000")
+//     doneStyle = lipgloss.Color("#29FF03")
+// )
+//
+// type item struct {
+// 	title, desc, done string
+// }
+//
+// func (i item) Title() string       { return i.title }
+// func (i item) Description() string { return i.desc }
+// func (i item) FilterValue() string { return i.done }
+//
+// type model struct {
+// 	list list.Model
+// }
+//
+// func (m model) Init() tea.Cmd {
+// 	return nil
+// }
+//
+// func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+// 	switch msg := msg.(type) {
+// 	case tea.KeyMsg:
+// 		if msg.String() == "ctrl+c" {
+// 			return m, tea.Quit
+// 		}
+// 	case tea.WindowSizeMsg:
+// 		h, v := appStyle.GetFrameSize()
+// 		m.list.SetSize(msg.Width-h, msg.Height-v)
+// 	}
+//
+// 	var cmd tea.Cmd
+// 	m.list, cmd = m.list.Update(msg)
+// 	return m, cmd
+// }
+//
+// func (m model) View() string {
+// 	return appStyle.Render(m.list.View())
+// }
+//
+// func Main() {
+//     results := bubbletea_queries.List()
+//     items := []list.Item{}
+//
+//     for _, r := range results {
+//         items = append(items, item{title: r["task"], desc: r["id"], done: r["done"]})
+//     }
+//     
+//     delegate := list.NewDefaultDelegate()
+//     delegate.Styles.SelectedTitle = delegate.Styles.SelectedTitle.Foreground(notDoneStyle)
+//
+// 	m := model{list: list.New(items, delegate, 0, 0)}
+// 	m.list.Title = "TODO"
+//     m.list.Styles.Title = titleStyle
+//
+// 	p := tea.NewProgram(m, tea.WithAltScreen())
+//
+// 	if _, err := p.Run(); err != nil {
+// 		fmt.Println("Error running program:", err)
+// 		os.Exit(1)
+// 	}
+// }
