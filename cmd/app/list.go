6d0881da5ba8c093fa2d1f1ea45eafd0a6d54f0a
CodyBense <codybense@gmail.com>
2024-08-15 21:18:59 -0400

    fixed sizing and added with alt screen

--- cmd/app/list.go
+++ cmd/app/list.go
@@ -9,7 +9,20 @@ import (
 	"github.com/charmbracelet/lipgloss"
 )
 
-// Custom Item
+/* STYLING */
+
+var (
+    columnStyle = lipgloss.NewStyle().
+            Padding(1, 2)
+    focusedStyle = lipgloss.NewStyle().
+            Padding(1, 2).
+            Border(lipgloss.RoundedBorder()).
+            BorderForeground(lipgloss.Color("62"))
+    helpStyle = lipgloss.NewStyle().
+            Foreground(lipgloss.Color("241"))
+)
+
+/* CUSTOM ITEM */
 
 func (t Task) FilterValue() string {
     return t.title
@@ -23,21 +36,38 @@ func (t Task) Description() string {
     return t.description
 }
 
-// Main model
+/* MAIN MODEL */
 
 type Model struct {
+    loaded      bool
     focused     status
     lists       []list.Model
     err         error
-    loaded      bool
+    quitting    bool
 }
 
 func New() *Model {
     return &Model{}
 }
 
+func (m *Model) Next() {
+    if m.focused == done {
+        m.focused = todo
+    } else {
+        m.focused++
+    }
+}
+
+func (m *Model) Prev() {
+    if m.focused == todo {
+        m.focused = done
+    } else {
+        m.focused--
+    }
+}
+
 func (m *Model) initLists(width, height int) {
-    defaultList := list.New([]list.Item{}, list.NewDefaultDelegate(), width/divsor, height)
+    defaultList := list.New([]list.Item{}, list.NewDefaultDelegate(), width/divsor, height/2)
     defaultList.SetShowHelp(false)
     m.lists = []list.Model{defaultList, defaultList, defaultList}
 
@@ -68,8 +98,24 @@ func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
     switch msg := msg.(type) {
     case tea.WindowSizeMsg:
         if !m.loaded {
+            columnStyle.Width(msg.Width / divsor)
+            focusedStyle.Width(msg.Width / divsor)
+            columnStyle.Height(msg.Height - divsor)
+            focusedStyle.Height(msg.Height - divsor)
             m.initLists(msg.Width, msg.Height)
             m.loaded = true
+        }
+    case tea.KeyMsg:
+        switch msg.String(){
+        case "ctrl+c", "q":
+            m.quitting = true
+            return m, tea.Quit
+        case "left", "h":
+            m.Prev()
+            return m, nil
+        case "right", "l":
+            m.Next()
+            return m, nil
         }
     }
     var cmd tea.Cmd
@@ -78,13 +124,38 @@ func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 }
 
 func (m Model) View() string {
+    if m.quitting {
+        return ""
+    }
     if m.loaded {
-        return lipgloss.JoinHorizontal(
-            lipgloss.Left,
-            m.lists[todo].View(),
-            m.lists[inProgress].View(),
-            m.lists[done].View(),
-        )
+        todoView := m.lists[todo].View()
+        inProgressView := m.lists[inProgress].View()
+        doneView := m.lists[done].View()
+
+        switch m.focused {
+        case inProgress:
+            return lipgloss.JoinHorizontal(
+                lipgloss.Left,
+                columnStyle.Render(todoView),
+                focusedStyle.Render(inProgressView),
+                columnStyle.Render(doneView),
+            )
+        case done:
+            return lipgloss.JoinHorizontal(
+                lipgloss.Left,
+                columnStyle.Render(todoView),
+                columnStyle.Render(inProgressView),
+                focusedStyle.Render(doneView),
+            )
+        default:
+            return lipgloss.JoinHorizontal(
+                lipgloss.Left,
+                focusedStyle.Render(todoView),
+                columnStyle.Render(inProgressView),
+                columnStyle.Render(doneView),
+            )
+        }
+
     } else {
         return "loading..."
     }
@@ -92,7 +163,7 @@ func (m Model) View() string {
 
 func Main() {
     m := New()
-    p := tea.NewProgram(m)
+    p := tea.NewProgram(m, tea.WithAltScreen())
     if _,err := p.Run(); err != nil {
         fmt.Println(err)
         os.Exit(1)
