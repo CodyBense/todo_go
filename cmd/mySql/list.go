03eb9266bbe7dbf1955857edcc825bae35425e44
CodyBense <codybense@gmail.com>
2024-08-20 17:52:08 -0400

    updated list func

--- cmd/mySql/list.go
+++ cmd/mySql/list.go
@@ -3,15 +3,24 @@ package mySql
 import (
 	"fmt"
 	"log"
+	"os"
 
 	"database/sql"
 
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/joho/godotenv"
 )
 
 func List() {
+    // Load environment variable
+    err := godotenv.Load()
+    if err != nil {
+        log.Fatalln("Error loading .env file")
+    }
+    connection := os.Getenv("MYSQL_CONNECTION")
+
     // Open mysql connection
-    db, err := sql.Open("mysql", "root:ZSe45rdx##@tcp(192.168.1.129:3306)/todo")
+    db, err := sql.Open("mysql", connection)
     if err != nil {
         log.Fatalf("impossible to create the connection: %s", err)
     }
@@ -25,28 +34,72 @@ func List() {
     
     // sql.Connect()
 
-    // Header
-    fmt.Printf("id\t|\tdone\t|\ttask\n")
-
+    // database variables
     var (
-        id int
         task string
-        done bool
+        description string
     )
 
+    // To DO Header
+    fmt.Println("To Do")
+
     // Conduct query
-    rows, err := db.Query("SELECT * FROM list")
+    rows, err := db.Query("SELECT * FROM todo")
     if err != nil {
         log.Fatalf("not able to conduct query: %s", err)
     }
     defer rows.Close()
 
     for rows.Next() {
-        err := rows.Scan(&id, &task, &done)
+        err := rows.Scan(&task, &description)
         if err != nil {
             log.Fatal(err)
         }
-        fmt.Printf("%d \t|\t%v\t|\t%s\n", id, done, task)
+        fmt.Printf("Task: %s\n\t%s\n", task, description)
+    }
+    err = rows.Err()
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    // To DO Header
+    fmt.Printf("\n\nIn Progress\n")
+
+    // Conduct query
+    rows, err = db.Query("SELECT * FROM inProgress")
+    if err != nil {
+        log.Fatalf("not able to conduct query: %s", err)
+    }
+    defer rows.Close()
+
+    for rows.Next() {
+        err := rows.Scan(&task, &description)
+        if err != nil {
+            log.Fatal(err)
+        }
+        fmt.Printf("Task: %s\n\t%s\n", task, description)
+    }
+    err = rows.Err()
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    // To DO Header
+    fmt.Printf("\n\nDone\n")
+
+    // Conduct query
+    rows, err = db.Query("SELECT * FROM done")
+    if err != nil {
+        log.Fatalf("not able to conduct query: %s", err)
+    }
+    defer rows.Close()
+
+    for rows.Next() {
+        err := rows.Scan(&task, &description)
+        if err != nil {
+            log.Fatal(err)
+        }
+        fmt.Printf("Task: %s\n\t%s\n", task, description)
     }
     err = rows.Err()
     if err != nil {
